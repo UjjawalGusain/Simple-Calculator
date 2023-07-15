451bab6e789b1aef9e411f9498fef8fd248fcea0
Ujjawal Gusain <ujjawalgusain31@gmail.com>
2023-07-16 01:36:36 +0530

    Add all HTML and CSS

---
+++ app.js
@@ -0,0 +1,44 @@
+let display = document.querySelector('.display');
+let buttons = document.querySelectorAll('.buttons');
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".nine");
+let zero = document.querySelector(".zero");
+let multiply = document.querySelector(".multiply");
+let divide = document.querySelector(".divide");
+let plus = document.querySelector(".plus");
+let minus = document.querySelector(".minus");
+let decimal = document.querySelector(".decimal");
+let del = document.querySelector(".del");
+let allClear = document.querySelector(".all-clear");
+
+let buttonsArr = Array.from(buttons);
+
+// let buttons = {
+// one : 1,
+// two : 2,
+// three : 3,
+// four : 4,
+// five : 5,
+// six : 6,
+// seven : 7,
+// eight : 8,
+// nine : 9,
+// zero : 0,
+// equals 
+// multiply : document.querySelector(".multiply"),
+// divide : document.querySelector(".divide"),
+// plus : document.querySelector(".plus"),
+// minus : document.querySelector(".minus"),
+// decimal : document.querySelector(".decimal"),
+// del : document.querySelector(".del"),
+// allClear : document.querySelector(".all-clear"),
+// }
+
+
